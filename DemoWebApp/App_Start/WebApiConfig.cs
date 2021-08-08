9677b2b0910497da206a2e942619afd4a9576552
Girish Nair <nairgirish100@gmail.com>
2021-08-08 18:00:56 +0530

    initial commit

---
+++ DemoWebApp/App_Start/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DemoWebApp
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Indent JSON data
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            
+            //Use Camel case
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
---
+++ DemoWebApp/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DemoWebApp.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        IList<string> listData = new List<string>()
+        {
+            "value1", "value2","value3"
+        };
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            return listData;
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return listData[id];
+        }
+
+        // POST api/values
+        public void Post([FromBody] string value)
+        {
+            listData.Add(value);
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+            listData[id] = value;
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            listData.RemoveAt(id);
+
+        }
+    }
+}
